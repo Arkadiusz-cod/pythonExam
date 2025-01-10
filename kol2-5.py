739f92e1db8a6c2fa2f7769486eb9a9eb8b524c8
Arkadiusz-Zaremba <a_aroo@o2.pl>
2025-01-10 20:48:38 +0100

    Create kol2-5.py

---
+++ kol2-5.py
@@ -0,0 +1,36 @@
+#lista
+#fruits = ["apple", "orange", "banana", "coconut"]
+#print(dir(fruits))
+#print(help(fruits))
+#print(len(fruits))
+#print("pineapple" in fruits)
+#print(fruits[::-1])
+#fruits[1] = "pineapple"
+#fruits.append("pineapple")
+#fruits.remove("apple")
+#fruits.insert(0, "pineapple")
+#fruits.sort()
+#fruits.reverse()
+#fruits.clear()
+#print(fruits.index("coconut"))
+#print(fruits.count("pineapple"))
+#for fruit in fruits:
+ #   print(fruit)
+ #set
+#fruits = {"apple", "orange", "banana", "coconut"} #-set
+#print(dir(fruits))
+#print("pineapple" in fruits)
+#fruits.add("pineapple")
+#fruits.remove("apple")
+#fruits.pop()
+#print(fruits)
+#tuples
+fruits = ("apple", "orange", "banana", "coconut")
+#print(dir(fruits))
+#print(help(fruits))
+#print(len(fruits))
+#print("pineapple" in fruits)
+print(fruits.index("apple"))
+print(fruits.count("coconut"))
+for fruit in fruits:
+    print("fruit")
